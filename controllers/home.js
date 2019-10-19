fb21e15fd3091280309739426f799b8868cbab1a
Aditya Dewan <iit2015097@iiita.ac.in>
2019-10-19 13:31:01 +0100

    remove extra bits

--- controllers/home.js
+++ controllers/home.js
@@ -4,6 +4,21 @@
  */
 exports.index = (req, res) => {
   res.render('home', {
-    title: 'Home'
+    title: 'Home',
+    users: [{
+      name: 'Eion Luca',
+      img_url: 'https://ca.slack-edge.com/TP7BJF3FV-UPJRE3AJC-7594b6e78e3f-72',
+      id: 1
+    },
+    {
+      name: 'Paulius Kuzmickas',
+      img_url: 'https://ca.slack-edge.com/TP7BJF3FV-UPJRE3AJC-7594b6e78e3f-72',
+      id: 2
+    },
+    {
+      name: 'Aditya Dewan',
+      img_url: 'https://ca.slack-edge.com/TP7BJF3FV-UPJRE3AJC-7594b6e78e3f-72',
+      id: 3
+    }]
   });
 };
