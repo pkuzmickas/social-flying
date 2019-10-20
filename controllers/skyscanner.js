ae891f1a984ab7e464a30151aaeaa1fe3abec207
Ioan Luca <ioan.luca7@gmail.com>
2019-10-20 02:44:25 +0100

    [ skyscanner ] add extractor

--- controllers/skyscanner.js
+++ controllers/skyscanner.js
@@ -9,7 +9,7 @@ const options = {
 };
 
 const pollInterval = 500;
-let pollerObj, sessionData = {};
+let pollerObj;
 let timerCleared = false;
 
 exports.getFlights = (req, res) => {
@@ -17,7 +17,7 @@ exports.getFlights = (req, res) => {
     axios.post(url, req.query, options)
         .then(response => {
             console.log(response.data);
-            poll(res, response.data.session_id);
+            poll(res, response.data, req.query);
         })
         .catch(error => {
             console.log('error', 'could not subscribe');
@@ -26,33 +26,48 @@ exports.getFlights = (req, res) => {
 }
 
 
-poll = (ogRes, session_id) => {
-    pollerObj = setInterval(() => { fetchCurrentStatus(ogRes, session_id) }, pollInterval);
+poll = (ogRes, data, query) => {
+    pollerObj = setInterval(() => { fetchCurrentStatus(ogRes, data, query) }, pollInterval);
 }
 
-fetchCurrentStatus = (res, session_id) => {
+fetchCurrentStatus = (res, data, query) => {
     if (timerCleared === true) {
         return;
     }
 
-    axios.get(url + '?session_id=' + session_id, options)
+    axios.get(url + '?session_id=' + data.session_id, options)
         .then(response => {
             console.log((new Date()).getTime(), 'polled successfully');
             console.log('status is ' + response.data.Status)
-            sessionData[session_id] = response.Itineraries;
             if (response.data.Status === 'UpdatesComplete') {
                 clearInterval(pollerObj);
                 timerCleared = true;
-                res.json(response.data);
+                flight = extractFlight(response.data, query);
+                res.json(flight);
             }
         })
         .catch(err => {
             console.log('error', 'polled failed');
             clearInterval(pollerObj);
-            if(!timerCleared) {
+            if (!timerCleared) {
                 res.status(500).send({ error: err });
             }
             timerCleared = true;
         });
+}
+
+
+const extractFlight = (data, query) => {
+    let { flightRef } = query;
+    // let unique = [...new Set(myArray)]; 
+    let oks = data.Legs
+        .filter(({ FlightNumbers }) =>
+            FlightNumbers.some(({ FlightNumber }) => FlightNumber === flightRef))
+        .map(({ Id }) => Id);
+
+    let itins = data.Itineraries
+        .filter(({ OutboundLegId }) => oks.includes(OutboundLegId));
+    flight = {...query, query: data.Query, itins};
+    return flight;
 }
 
